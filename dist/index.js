e65e79888e596a04e7bc68e868e77498692901b5
Tykok <treportelie12@gmail.com>
2022-11-22 16:18:48 +0100

    [Fix] : Add 'continue-on-error' property to allow failure

--- dist/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const dictionary_function_1 = require("./utils/dictionary-function");
-console.log((0, dictionary_function_1.getByTraditional)('C盤'));
