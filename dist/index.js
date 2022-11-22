62a5eeab58691e0550245ad4511e6fefb3aef492
Tykok <treportelie12@gmail.com>
2022-11-22 09:12:32 +0100

    [Fix] : Add dist directory

---
+++ dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const dictionnary_function_1 = require("./utils/dictionnary-function");
+console.log((0, dictionnary_function_1.getByTraditional)('C盤'));
