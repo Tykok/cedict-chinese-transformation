d3e4ddfbc306c95b52b611614a4c210e9e31792b
Tykok <treportelie12@gmail.com>
2023-01-26 09:37:23 +0100

    [Fix] Fix the workflow

--- dist/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const dictionary_function_1 = require("./utils/dictionary-function");
-exports = { getByTraditional: dictionary_function_1.getByTraditional, getByEnglish: dictionary_function_1.getByEnglish, getByPinyin: dictionary_function_1.getByPinyin, getBySimplified: dictionary_function_1.getBySimplified };
