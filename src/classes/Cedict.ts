b5d45996b63f8da00f47dd69665e55cf63cac7e3
Tykok <treportelie12@gmail.com>
2023-06-12 17:32:15 +0200

    [Fix] Return all occurence of a given string

--- src/classes/Cedict.ts
+++ src/classes/Cedict.ts
@@ -1,35 +1,37 @@
-import cedict from '../../data/cedict.json';
-import ChineseWord from '../types/ChineseWord';
+import cedict from '../../data/cedict.json'
+import ChineseWord from '../types/ChineseWord'
 
 class Cedict {
   static getCedict(): ChineseWord[] {
     return cedict;
   }
 
-  static getByTraditional(character: string): ChineseWord | undefined {
-    return Cedict.getCedict().find((word) => word.traditional === character);
+  static getByTraditional(character: string): ChineseWord[] | undefined {
+    return Cedict.getCedict().filter((word) => {
+      return word.traditional.toUpperCase() === character.toUpperCase()
+    })
   }
 
-  static getBySimplified(character: string): ChineseWord | undefined {
-    return Cedict.getCedict().find((word) => word.simplified === character);
+  static getBySimplified(character: string): ChineseWord[] | undefined {
+    return Cedict.getCedict().filter((word) => {
+      return word.simplified.toUpperCase() === character.toUpperCase()
+    })
   }
 
-  static getByPinyin(character: string): ChineseWord | undefined {
-    return Cedict.getCedict().find((word) => word.pinyin === character);
+  static getByPinyin(character: string): ChineseWord[] | undefined {
+    return Cedict.getCedict().filter((word) => {
+      return word.pinyin.toUpperCase() === character.toUpperCase()
+    })
   }
 
   static getByEnglish(sentence: string): ChineseWord[] | undefined {
     return Cedict.getCedict().filter((word) => {
-      for (const english of word.english) {
-        if (english.includes(sentence)) return true;
-      }
-      return false;
-    });
-  }
-
-  static allOccurenceOfTraditional(traditional: string): ChineseWord[] | undefined {
-    return Cedict.getCedict().filter((word) => word.traditional.includes(traditional));
+      for (const english of word.english)
+        if (english.toUpperCase().includes(sentence.toUpperCase())) return true
+        
+      return false
+    })
   }
 }
 
-export default Cedict;
+export default Cedict
